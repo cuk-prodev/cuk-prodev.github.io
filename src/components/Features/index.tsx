6b9ee02ce7b43d292599c0a1cc49c8be027ef602
echorockers06 <echorockers06@gmail.com>
2025-06-24 16:07:57 +0700

    u

---
+++ src/components/Features/index.tsx
@@ -0,0 +1,25 @@
+import SectionTitle from "../Common/SectionTitle";
+import SingleFeature from "./SingleFeature";
+import featuresData from "./featuresData";
+
+const Features = () => {
+  return (
+    <section className="pb-8 pt-20 dark:bg-dark lg:pb-[70px] lg:pt-[120px]">
+      <div className="container">
+        <SectionTitle
+          subtitle="Features"
+          title="Main Features Of Play"
+          paragraph="Discover the core features that make Play powerful, fast, and ready for modern development. From performance to design, we’ve crafted everything to help you build better — faster."
+        />
+
+        <div className="-mx-4 mt-12 flex flex-wrap lg:mt-20">
+          {featuresData.map((feature, i) => (
+            <SingleFeature key={i} feature={feature} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Features;
