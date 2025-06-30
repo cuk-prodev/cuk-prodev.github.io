18da8e60cb3cae7e2c0a2c18ee56aaba38a40b02
echorockers06 <echorockers06@gmail.com>
2025-06-30 16:01:51 +0700

    uuu

--- next.config.js
+++ next.config.js
@@ -1,11 +1,12 @@
+// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
   },
-  basePath: "/cuk-prodev",          // <- sesuai nama repo
-  assetPrefix: "/cuk-prodev",       // <- supaya file CSS/JS bisa ditemukan
+  basePath: "",       // ✅ kosongkan
+  assetPrefix: "",     // ✅ kosongkan
 };
 
 module.exports = nextConfig;
