83989eaa97397195597ec466ce2014c5cdcf9a3e
echorockers06 <echorockers06@gmail.com>
2025-06-30 16:52:13 +0700

    uuu

---
+++ app/page.tsx
@@ -0,0 +1,23 @@
+import Hero from "../components/Hero";
+import Features from "../components/Features";
+import About from "../components/About";
+import Contact from "../components/Contact";
+import ScrollUp from "../components/Common/ScrollUp";
+import Head from "./head";
+
+export default function HomePage() {
+  return (
+    <>
+      <Head/>
+     
+      <main>
+        <ScrollUp />
+        <Hero />
+        <Features />
+        <About />
+        <Contact />
+      </main>
+    </>
+    
+  );
+}
