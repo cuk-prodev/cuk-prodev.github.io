169b0014625dea08d5ff8717e411b144395945c3
echorockers06 <echorockers06@gmail.com>
2025-07-07 12:12:57 +0700

    update : settingan

--- app/head.tsx
+++ app/head.tsx
@@ -6,6 +6,7 @@ export default function Head() {
           name="description"
           content="🔴 CUK (Create Until Kickass) Solusi Digital untuk Bisnismu — Dari Ide Sampai Jadi Karya Kickass. CUK adalah tim developer berpengalaman di bidang Web, Mobile, dan Sistem Backend. Dengan lebih dari 10 tahun pengalaman, kami telah membangun berbagai solusi digital mulai dari sistem informasi, HRIS, POS, hingga platform marketplace dan media sosial. Cepat. Scalable. Kickass. Kami hadir untuk bantu mewujudkan ide dan kebutuhan bisnismu jadi aplikasi nyata yang tangguh, modern, dan siap bersaing."
         /> */}
+        <meta name="google-site-verification" content="NCNiHtize2Ll5bxpGesyEebKLH5TgTaP-gE5IDGNL8M" />
         <meta name="description" content="CUK PRODEV menyediakan jasa pembuatan website, aplikasi Android, dan sistem digital seperti absensi, booking room, dan dashboard IoT." />
         <meta name="keywords" content="jasa pembuatan website, jasa pembuatan aplikasi Android, jasa website murah, freelancer IT, jasa pembuatan sistem absensi, jasa aplikasi IoT, web developer Jakarta, jasa React Native, developer GitHub Pages, jasa dashboard admin, jasa integrasi API, programmer freelance" />
         <meta name="author" content="CUK PRODEV" />
